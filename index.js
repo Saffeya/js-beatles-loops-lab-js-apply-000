b7d87311909226245b2833883d9253c80638fbc5
Saffeya Hinkson <sthinkson@hotmail.com>
2018-01-13 17:22:17 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,6 @@
+function theBeatlesPlay (musicians,instruments){
+  ["John Lennon plays guitar",
+'Paul McCartney plays Bass Guitar',
+'George Harrison plays Lead Guitar',
+'Ringo Starr plays Drums']
+}
